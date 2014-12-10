ce37b0ad78804fe559e98f5b9c67ab66b6f2af5d
Luciano Guzinski <lucianoguzinski@gmail.com>
2014-12-09 22:48:39 -0200

    Tela de login

--- src/Sindilojas/CobrancaBundle/Controller/DefaultController.php
+++ src/Sindilojas/CobrancaBundle/Controller/DefaultController.php
@@ -3,17 +3,27 @@
 namespace Sindilojas\CobrancaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    
+    /**
+     * @Route("/", name="_teste")
+     */
+    public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
-        
-        $clientes = $em->getRepository("SindilojasCobrancaBundle:Cliente")->findAll();
-        $lojas = $em->getRepository("SindilojasCobrancaBundle:Loja")->findAll();
-        $dividas = $em->getRepository("SindilojasCobrancaBundle:Divida")->findAll();
-
-        return $this->render('SindilojasCobrancaBundle:Default:index.html.twig', array('name' => $name));
+        return new Response("Index");
+    }
+    
+    /**
+     * @Route("/login", name="_login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        return array();
     }
 }
